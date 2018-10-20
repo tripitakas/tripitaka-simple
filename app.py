2a4c16f0ea73816a9df9a0ab63760352fa56221e
Zhang Yungui <rhcad@hotmail.com>
2018-10-20 08:31:33 +0800

    Auto lock or unlock pages

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ from tornado import netutil, process
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options
 import logging
-from os import path, listdir
+from os import path, listdir, remove
 import json
+import random
+import re
+import time
+import datetime
+
 
 BASE_DIR = path.dirname(__file__)
 define('port', default=8001, help='run port', type=int)
@@ -31,7 +36,18 @@ def save_json(obj, filename, sort_keys=False):
         json.dump(obj, f, ensure_ascii=False, sort_keys=sort_keys)
 
 
-class MainHandler(RequestHandler):
+class BaseHandler(RequestHandler):
+    def set_default_headers(self):
+        h = 'Content-Type,Host,X-Forwarded-For,X-Requested-With,User-Agent,Cache-Control,Cookies'
+        self.set_header('Cache-Control', 'no-cache')
+        self.set_header('Access-Control-Allow-Headers', h)
+
+    def get_ip(self):
+        ip = self.request.headers.get('x-forwarded-for') or self.request.remote_ip
+        return ip and re.sub(r'^::\d$', '', ip[:15]) or 'localhost'
+
+
+class MainHandler(BaseHandler):
     URL = r'/'
 
     def get(self):
@@ -39,21 +55,47 @@ class MainHandler(RequestHandler):
         self.render('index.html', kinds=kinds, index=index)
 
 
-class PagesHandler(RequestHandler):
-    URL = r'/(block|column|char)/([A-Z]{2})'
+class PagesHandler(BaseHandler):
+    URL = r'/(block|column|char)/([A-Z]{2})/?'
 
     def get(self, pos, kind):
         def get_icon(p):
             return path.join('icon', *p.split('_')[:-1], p + '.jpg')
 
+        self.unlock_timeout(pos)
         index = load_json(path.join('static', 'index.json'))
-        pages = index[kind]
+        pages = self.pick_pages(pos, index[kind], 12)
         pos_type = '字切分' if pos == 'char' else '栏切分' if pos == 'block' else '列切分'
         self.render('pages.html', kinds=kinds, pages=pages,
                     pos_type=pos_type, pos=pos, kind=kind, get_icon=get_icon)
 
+    @staticmethod
+    def unlock_timeout(pos):
+        lock_path = path.join(BASE_DIR, 'data', 'lock', pos)
+        now = time.time()
+        for fn in listdir(lock_path):
+            filename = path.join(lock_path, fn)
+            if '_' in fn:
+                t = path.getctime(filename)
+                if now - t > 3600:
+                    with open(filename) as f:
+                        text = f.read()
+                    if 'saved' not in text:
+                        remove(filename)
+                        logging.warning('%s unlocked: %s' % (fn, text))
 
-class CutProofHandler(RequestHandler):
+    @staticmethod
+    def get_lock_file(pos, name):
+        return path.join(BASE_DIR, 'data', 'lock', pos, name)
+
+    @staticmethod
+    def pick_pages(pos, pages, count):
+        pages = [p for p in pages if not path.exists(PagesHandler.get_lock_file(pos, p))]
+        random.shuffle(pages)
+        return sorted(pages[:count])
+
+
+class CutProofHandler(BaseHandler):
     URL = r'/(block|column|char)/([A-Z]{2})/(\w{4,20})'
 
     def get(self, pos, kind, name):
@@ -63,7 +105,17 @@ class CutProofHandler(RequestHandler):
         name = kind + '_' + name
         filename = path.join(BASE_DIR, 'static', 'char-pos', *name.split('_')[:-1], name + '.json')
         page = load_json(filename)
+        if not page:
+            return self.write('error:{0} 页面不存在'.format(name))
         page[pos + 's'] = json.dumps(page[pos + 's'])
+
+        lock_file = PagesHandler.get_lock_file(pos, name)
+        if path.exists(lock_file):
+            with open(lock_file) as f:
+                if self.get_ip() not in f.read():
+                    return self.write('error:别人已锁定了本页面，请返回选择其他页面。')
+        with open(lock_file, 'w') as f:
+            f.write(self.get_ip())
         self.render('char_cut.html' if pos == 'char' else 'block_cut.html',
                     pos_type='字切分' if pos == 'char' else '栏切分' if pos == 'block' else '列切分',
                     page=page, pos=pos, kind=kind, **page, get_img=get_img)
@@ -71,13 +123,22 @@ class CutProofHandler(RequestHandler):
     def post(self, pos, kind, name):
         filename = path.join(BASE_DIR, 'static', 'char-pos', *name.split('_')[:-1], name + '.json')
         page = load_json(filename)
+        submit = self.get_body_argument('submit') == 'true';
         boxes = json.loads(self.get_body_argument('boxes'))
         assert isinstance(boxes, list)
         if page[pos + 's'] != boxes:
             page[pos + 's'] = boxes
             save_json(page, filename)
             logging.info('%d boxes saved: %s' % (len(boxes), name))
-        self.write('ok')
+
+        lock_file = PagesHandler.get_lock_file(pos, name)
+        with open(lock_file, 'w') as f:
+            f.write(self.get_ip() + '\nsaved')
+
+        if submit:
+            pages = PagesHandler.pick_pages(pos, load_json(path.join('static', 'index.json'))[kind], 1)
+            self.write('jump:' + pages[0][3:] if pages else 'error:本类切分已全部校对完成。')
+        self.write('')
 
 
 def make_app():
@@ -92,7 +153,7 @@ if __name__ == '__main__':
     options.parse_command_line()
     try:
         app = make_app()
-        server = HTTPServer(app)
+        server = HTTPServer(app, xheaders=True)
         if options.debug:
             server.listen(options.port)
             fork_id = 0
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
-[[ -d log ]] || mkdir log
-sudo pkill -f 'python app.py --port=80'
+sudo pkill -f 'python app.py'
 find . -name "*.pyc" | sudo xargs rm -rf
 sudo nohup python app.py --port=80 >> log/app.log 2>&1 &
