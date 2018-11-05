71a3a5409b91ff1bdfb322d28c81cfcda2ac9096
Zhang Yungui <rhcad@hotmail.com>
2018-11-05 21:14:45 +0800

    Add CutHandler.save to support saving multiple pages

--- controller/cut.py
+++ controller/cut.py
@@ -91,7 +91,8 @@ class PagesHandler(BaseHandler):
 
 
 class CutHandler(BaseHandler):
-    URL = r'/(block|column|char)/([A-Z]{2})/(\w{4,20})'
+    URL = [r'/(block|column|char)/([A-Z]{2})/(\w{4,20})',
+           r'/(block|column|char)/([A-Z]{2})/?']
 
     def get(self, pos, kind, name):
         def get_img(p):
@@ -118,12 +119,35 @@ class CutHandler(BaseHandler):
                     pos_type='字切分' if pos == 'char' else '栏切分' if pos == 'block' else '列切分',
                     page=page, pos=pos, kind=kind, **page, get_img=get_img)
 
-    def post(self, pos, kind, name):
-        filename = path.join(BASE_DIR, 'static', 'char-pos', *name.split('_')[:-1], name + '.json')
-        page = load_json(filename)
+    def post(self, pos, kind, name=None):
+        """
+        保存一个或多个页面的切分校对数据.
+
+        保存一个页面时 name 为页名，请求体中需要有 boxes 框数组. 保存多个页面时 name 为空，请求体的 boxes 为页与框数组的字典.
+        如果在请求体中指定了 submit 属性，则会输出下一个校对任务的页名（jump:name 格式，无藏别）.
+        :param pos: 校对类型，block 为栏切分，column 为列切分，char 为字框切分
+        :param kind: 藏别，例如 GL、JX
+        :param name: 页名，例如 GL_1047_1_5，请求体中需要有 boxes 框数组. 如果页名为空，则 boxes 为{name: boxes, ...}字典
+        :return: None
+        """
         submit = self.get_body_argument('submit') == 'true'
         boxes = json.loads(self.get_body_argument('boxes'))
-        assert isinstance(boxes, list)
+        assert name or type(boxes) == dict
+        if name:
+            self.save(pos, name, boxes)
+        else:
+            for name, boxes in boxes.items():
+                self.save(pos, name, boxes)
+
+        if submit:
+            pages = PagesHandler.pick_pages(pos, load_json(path.join('static', 'index.json'))[kind], 1)[0]
+            self.write('jump:' + pages[0][3:] if pages else 'error:本类切分已全部校对完成。')
+        self.write('')
+
+    def save(self, pos, name, boxes):
+        filename = path.join(BASE_DIR, 'static', 'char-pos', *name.split('_')[:-1], name + '.json')
+        page = load_json(filename)
+        assert page and isinstance(boxes, list)
         if page[pos + 's'] != boxes:
             page[pos + 's'] = boxes
             save_json(page, filename)
@@ -141,8 +165,3 @@ class CutHandler(BaseHandler):
         with open(lock_file, 'w') as f:
             text += [self.get_ip(), self.current_user, get_date_time(), 'saved']
             f.write('\n'.join(text))
-
-        if submit:
-            pages = PagesHandler.pick_pages(pos, load_json(path.join('static', 'index.json'))[kind], 1)[0]
-            self.write('jump:' + pages[0][3:] if pages else 'error:本类切分已全部校对完成。')
-        self.write('')
