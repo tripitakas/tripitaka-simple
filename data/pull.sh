b11fdfccc541ddf2c9b1b74b6fb415fac6c5ba90
Zhang Yungui <rhcad@hotmail.com>
2018-12-03 18:19:39 +0800

    Remove pull.sh

--- data/pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-rm -fr lock char-pos
-scp -r sm@47.95.216.233:/home/sm/cut_proof/data/lock ./lock
-scp -r sm@47.95.216.233:/home/sm/cut_proof/static/char-pos ./char-pos
-# sshpass -p password scp -r ...
-python sum_pos.py
