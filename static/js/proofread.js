1d95cd50180dc85eb9a5e509d54f51060717707b
Zhang Yungui <rhcad@hotmail.com>
2018-12-16 16:55:27 +0800

    单击异文中当浮动框“底”部出界时向上弹出；不让当前span成为块元素，改为仅让新加行成为块元素

--- static/js/proofread.js
+++ static/js/proofread.js
@@ -227,21 +227,23 @@ $(document).on('click', '.not-same', function (e) {
     if ($(this).hasClass('variant') && !$(this).hasClass('variant-highlight')) {
         return;
     }
+    var $dlg = $("#pfread-dialog");
     $("#pfread-dialog-cmp").text($(this).attr("cmp"));
     $("#pfread-dialog-ocr").text($(this).attr("ocr"));
     $("#pfread-dialog-slct").text("");
-    $("#pfread-dialog").offset({ top: $(this).offset().top + 40, left: $(this).offset().left - 4 });
-    $("#pfread-dialog").show();
+    $dlg.offset({ top: $(this).offset().top + 40, left: $(this).offset().left - 4 });
+    $dlg.show();
     
     //当弹框超出文字框时，向上弹出
     var r_h = $(".right").height();
-    var o_t = $("#pfread-dialog").offset().top;
-    $("#pfread-dialog").removeClass('dialog-common-t');
-    	$("#pfread-dialog").addClass('dialog-common');
-    if(o_t>r_h){
-    		$("#pfread-dialog").offset({ top: $(this).offset().top - 180, left: $(this).offset().left - 4 });
-    		$("#pfread-dialog").removeClass('dialog-common');
-    		$("#pfread-dialog").addClass('dialog-common-t');
+    var o_t = $dlg.offset().top;
+    var d_h = $dlg.height();
+    $dlg.removeClass('dialog-common-t');
+    $dlg.addClass('dialog-common');
+    if (o_t + d_h > r_h) {
+        $dlg.offset({ top: $(this).offset().top - 180, left: $(this).offset().left - 4 });
+        $dlg.removeClass('dialog-common');
+        $dlg.addClass('dialog-common-t');
     }
     
     // 设置当前异文
