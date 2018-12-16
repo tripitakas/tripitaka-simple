f01694037c3cacb93da68bc85f34527cbabf799e
Zhang Yungui <rhcad@hotmail.com>
2018-12-16 14:56:45 +0800

    十分钟内下载过就不再重新生成压缩包

--- controller/export.py
+++ controller/export.py
@@ -4,7 +4,7 @@
 from tornado import gen
 from tornado.concurrent import Future
 from tornado.ioloop import IOLoop
-from controller.base import BaseHandler
+from controller.base import BaseHandler, get_date_time
 from glob import glob
 from os import path
 import zipfile
@@ -17,7 +17,9 @@ class ExportHandler(BaseHandler):
     def get(self, pos, kind):
         def compress(future):
             files = glob(path.join(data_path, '**', '*.*'), recursive=True)
-            arc = zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_LZMA)
+            if path.exists(zip_file):
+                return future.set_result(len(files))
+            arc = zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_DEFLATED)
             count = 0
             for filename in files:
                 if filename.endswith('.json') or filename.endswith('.txt'):
@@ -36,7 +38,7 @@ class ExportHandler(BaseHandler):
         static_path = path.join(base_path, 'static')
         data_path = path.join(static_path if pos == 'txt' else path.join(static_path, 'pos'), pos, kind)
         zip_path = path.join(static_path, 'download')
-        zip_file = path.join(zip_path, '%s_%s.zip' % (pos, kind))
+        zip_file = path.join(zip_path, '%s-%s-%s.zip' % (pos, kind, get_date_time('%d%H%M')[:-1]))
 
         file_count = yield download()
         self.write('<a href="%s" target="_blank">%s</a> %d files' % (
